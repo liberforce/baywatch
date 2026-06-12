2e7efaf73c4c2f558a7fad1514fe8ee54bae3c9a
Luis Menina <liberforce@freeside.fr>
2026-06-12 04:10:10 +0200

    Add HTTP headers to try to not be detected as a bot

--- baywatch/tool.py
+++ baywatch/tool.py
@@ -21,7 +21,12 @@ REF_PAGE_PATH = "ref.html"
 
 def download_page(url: str) -> str:
     LOGGER.info("Downloading watched page")
-    response = requests.get(url, timeout=10)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "fr-FR,fr;q=0.8,en-US;q=0.5,en;q=0.3",
+    }
+    response = requests.get(url, headers=headers, timeout=10)
     return response.text
 
 
