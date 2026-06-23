a44e4139de489fe3362942013cff3b949384968a
Luis Menina <liberforce@freeside.fr>
2026-06-23 04:39:32 +0200

    Try to watch the child page

--- baywatch/app/baywatch.py
+++ baywatch/app/baywatch.py
@@ -1,3 +1,5 @@
+import re
+import copy
 import logging
 import random
 import time
@@ -45,32 +47,60 @@ class Bay:
         config.normalizer = make_normalizer("prefecture")
         self.config = config
 
-    def watch(self) -> None:
+    def do(self, action: str, page: Page) -> None:
+        if action == "tag":
+            tag(
+                page,
+                self.config.output_page_repository,
+            )
+        elif action == "send_email":
+            self.config.email.body = page.data
+            self.config.smtp.send_email(self.config.email)
+        elif action == "watch_child":
+            match = re.search(
+                r'[^"]*accepted_restrictions_for[^"]*',
+                page.data,
+                flags=re.MULTILINE,
+            )
+            if not match:
+                LOGGER.warning("Resource not found in page search")
+            else:
+                resource = match[0]
+                tmp_config = copy.deepcopy(self.config)
+                tmp_config.url = f"https://rdv.anct.gouv.fr/{resource}"
+                tmp_config.polling_period_in_sec = 10
+                Bay(tmp_config).watch(n_times=2, actions=["send_email"])
+
+    def watch(self, n_times: int = -1, actions: list[str] | None = None) -> None:
         LOGGER.info(f"Start polling (period={self.config.polling_period_in_sec}s)...")
+        if n_times < 0:
+            while 1:
+                self._watch(actions)
+        else:
+            for n in range(n_times):
+                self._watch(actions)
 
-        while 1:
-            new_page = Page(DataExtractor().extract(self.config.url))
+    def _watch(self, actions: list[str] | None = None) -> None:
+        new_page = Page(DataExtractor().extract(self.config.url))
 
-            if self.has_changed(
-                self.config.ref_page,
-                new_page,
-                self.config.normalizer,
-            ):
-                tag(
-                    new_page,
-                    self.config.output_page_repository,
-                )
-                self.config.ref_page = new_page
-                self.config.email.body = new_page.data
-                self.config.smtp.send_email(self.config.email)
+        if self.has_changed(
+            self.config.ref_page,
+            new_page,
+            self.config.normalizer,
+        ):
+            if actions is not None:
+                for action in actions:
+                    self.do(action, new_page)
 
-            # Add some jitter to try to not be flagged as a bot
-            jitter_upper_bound = self.config.polling_period_in_sec // 10
-            jitter_lower_bound = -jitter_upper_bound
-            jitter = random.randrange(jitter_lower_bound, jitter_upper_bound)
-            nice_polling_period = self.config.polling_period_in_sec + jitter
-            LOGGER.info(f"Trying again in {nice_polling_period}s...")
-            time.sleep(nice_polling_period)
+            self.config.ref_page = new_page
+
+        # Add some jitter to try to not be flagged as a bot
+        jitter_upper_bound = self.config.polling_period_in_sec // 10
+        jitter_lower_bound = -jitter_upper_bound
+        jitter = random.randrange(jitter_lower_bound, jitter_upper_bound)
+        nice_polling_period = self.config.polling_period_in_sec + jitter
+        LOGGER.info(f"Trying again in {nice_polling_period}s...")
+        time.sleep(nice_polling_period)
 
     def has_changed(
         self,
@@ -93,7 +123,8 @@ def main() -> None:
     config_loader = EnvConfigLoader()
     config = config_loader.load()
     bay = Bay(config)
-    bay.watch()
+    actions = ["tag", "send_email", "watch_child"]
+    bay.watch(actions=actions)
 
 
 if __name__ == "__main__":
